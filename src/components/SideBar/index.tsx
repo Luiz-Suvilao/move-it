db9bddc79f771807b09dc60d120df327f3864dce
Luiz Filipe da Silva de Jesus <infobase.lfsilva@terceiros.mag.com.br>
2021-02-28 22:38:22 -0300

    turning sidebar into a dumb component

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -4,37 +4,39 @@ import Link from 'next/link'
 
 import styles from './SideBar.module.css';
 
-const SideBar:React.FC = () => {
-	return (
-		<aside className={ styles.container }>
-			<div className={ styles.linkContainer }>
-				<nav>
-					<img
-						className={ styles.img } src="/icons/sideBarLogo.svg"
-						alt="Logo na sidebar. Move.it"
-					/>
+const SideBar:React.FC = () => (
+	<aside className={ styles.container }>
+		<div className={ styles.linkContainer }>
+			<nav>
+				<Link href='/'>
+					<a className={ styles.logo }>
+						<img
+							src="/icons/sideBarLogo.svg"
+							alt="Imagem de uma casa com link para voltar ao home"
+						/>
+					</a>
+				</Link>
 
-					<Link href='/'>
-						<a className={ styles.link }>
-							<img
-								src="/icons/house.svg"
-								alt="Imagem de uma casa com link para voltar ao home"
-							/>
-						</a>
-					</Link>
+				<Link href='/home'>
+					<a className={ styles.link }>
+						<img
+							src="/icons/house.svg"
+							alt="Imagem de uma casa com link para voltar ao home"
+						/>
+					</a>
+				</Link>
 
-					<Link href='/ranking'>
-						<a className={ styles.link }>
-							<img
-								src="/icons/ranking.svg"
-								alt="Imagem de uma bandeira com link para o ranking"
-							/>
-						</a>
-					</Link>
-				</nav>
-			</div>
-		</aside>
-	);
-}
+				<Link href='/ranking'>
+					<a className={ styles.link }>
+						<img
+							src="/icons/ranking.svg"
+							alt="Imagem de uma bandeira com link para o ranking"
+						/>
+					</a>
+				</Link>
+			</nav>
+		</div>
+	</aside>
+);
 
 export default SideBar;
